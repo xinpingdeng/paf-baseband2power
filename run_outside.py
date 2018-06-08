02384e6b76c9b0019ceabc0e0eb8eb350be76a90
xinpingdeng <xinping.deng@gmail.com>
2018-06-08 16:07:06 +0200

    run_outside.py

--- run_outside.py
+++ run_outside.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 
-import os
+import os, argparse
+
+# ./run_outside.py -a /beegfs/DENG/JUNE/ -b 100 -c 1 -d 9
+
+# Read in command line arguments
+parser = argparse.ArgumentParser(description='Convert baseband data into power')
+parser.add_argument('-a', '--ddir', type=str, nargs='+',
+                    help='Directory for data recording')
+parser.add_argument('-b', '--length', type=float, nargs='+',
+                    help='Observing length in seconds')
+parser.add_argument('-c', '--numa', type=int, nargs='+',
+                    help='On which numa node we do the work, 0 or 1')
+parser.add_argument('-d', '--nbeam', type=int, nargs='+',
+                    help='Number of beams.')
+
+args       = parser.parse_args()
+ddir       = args.ddir[0]
+length     = args.length[0]
+nbeam      = args.nbeam[0]
+numa       = args.numa[0]
+
+#ddir       = "/beegfs/DENG/JUNE/"
+#length     = 100
+#nbeam      = 9
+#numa       = 1
 
 hdir       = "/home/pulsar"
-ddir       = "/beegfs/DENG/JUNE/"
 memsize    = 80000000000
 uid        = 50000
 gid        = 50000
-numa       = 1
 gpu        = numa
-length     = 100
-nbeam      = 9
 dname      = "paf-baseband2power"
 conf_fname = "paf-baseband2power-stream.conf"
-
 visiblegpu  = 1
 memcheck    = 0
 
